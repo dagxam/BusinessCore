5e7e46855a5e0b1d2125d732092c1a36dbffb4cd
dagxam <dagxam@gmail.com>
2026-02-13 07:17:52 +0300

    Create GenderCheckTask.java

---
+++ src/main/java/com/businesscore/tasks/GenderCheckTask.java
@@ -0,0 +1,35 @@
+package com.businesscore.tasks;
+
+import com.businesscore.BusinessCore;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class GenderCheckTask extends BukkitRunnable {
+
+    private final BusinessCore plugin;
+
+    public GenderCheckTask(BusinessCore plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void run() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (player.hasPermission("authme.login")
+                    && !player.hasPermission("gender.selected")
+                    && !player.hasPermission("gender.menu.opened")) {
+
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+                        "lp user " + player.getName() + " permission set gender.menu.opened true");
+
+                int delay = plugin.getConfig().getInt("authme-gender-menu-delay-seconds", 2) * 20;
+                Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                    if (player.isOnline()) {
+                        plugin.getMenuManager().openMenu(player, "gender_select");
+                    }
+                }, delay);
+            }
+        }
+    }
+}
